6f61dd642b8a3a45c8ec10f9c77794bf95e6fc4a
Vadim <playvarian@gmail.com>
2021-09-16 22:51:19 +0300

    now server serves static files in ./front instead .

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const path = require('path')
 const app = express();
 
 console.log(__dirname)
-app.use(express.static(path.join(__dirname, '..', 'build')))
+app.use(express.static(path.join(__dirname, 'front', 'build')))
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'))
 })
 
 app.listen(process.env.PORT || 80, () => console.log('server started'))
